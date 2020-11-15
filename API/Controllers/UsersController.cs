ca21482f05f6c04e8a5d0d21081f35e152e71deb
Steve Thomas <stthomas4531@gmail.com>
2020-11-15 12:44:13 -0700

    Creating an Account Controller with a register endpoint

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,11 +5,11 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers {
-    [ApiController]
-    [Route ("api/[controller]")]
-    public class UsersController : ControllerBase {
+    
+    public class UsersController : BaseApiController {
         private readonly DataContext _context;
         public UsersController (DataContext context) 
         {
